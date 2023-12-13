e467b46ff191a5527a68a8cd3297566f062424a1
duyuitk16 <nguyenvuanhduy173@gmail.com>
2023-12-13 22:55:31 +0700

    Error Handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -4,16 +4,11 @@
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
 import { StatusCodes } from 'http-status-codes'
+
 const createNew = async (req, res, next) => {
   try {
-    console.log('req.body: ', req.body)
-    // throw new Error('duyuitk16 test error')
     res.status(StatusCodes.CREATED).json({ message: 'POST from Controller: API create a new board' })
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
-  }
+  } catch (error) { next(error) }
 }
 
 export const boardController = {
