a8f9398c8ec2e65bb6ba87306436d326dd7382c9
duyuitk16 <nguyenvuanhduy173@gmail.com>
2023-12-14 23:17:25 +0700

    Create Model

---
+++ src/models/boardModel.js
@@ -0,0 +1,25 @@
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+import Joi from 'joi'
+
+//Define collection (Name & Schema)
+const BOARD_COLLECTION_NAME = 'boards'
+const BOARD_COLLECTION_SCHEMA = Joi.object({
+  title: Joi.string().required().min(3).max(50).trim().strict(),
+  slug: Joi.string().required().min(3).trim().strict(),
+  description: Joi.string().required().min(3).max(256).trim().strict(),
+
+  columnOrderIds: Joi.array().items(Joi.string()).default([]),
+
+  createdAt: Joi.date().timestamp('javascript').default(Date.now),
+  updatedAt: Joi.date().timestamp('javascript').default(null),
+  _destroy: Joi.boolean().default(false)
+})
+
+export const boradModel = {
+  BOARD_COLLECTION_NAME,
+  BOARD_COLLECTION_SCHEMA
+}
