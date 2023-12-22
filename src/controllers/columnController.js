f37887f59aa7cbcdd6e3a44189233f33cce290fc
duyuitk16 <nguyenvuanhduy173@gmail.com>
2023-12-22 23:15:04 +0700

    Delete Column and Card

--- src/controllers/columnController.js
+++ src/controllers/columnController.js
@@ -22,7 +22,16 @@ const update = async (req, res, next) => {
   } catch (error) { next(error) }
 }
 
+const deleteItem = async (req, res, next) => {
+  try {
+    const columnId = req.params.id
+    const result = await columnService.deleteItem(columnId)
+    res.status(StatusCodes.OK).json(result)
+  } catch (error) { next(error) }
+}
+
 export const columnController = {
   createNew,
-  update
+  update,
+  deleteItem
 }
--- src/models/columnModel.js
+++ src/models/columnModel.js
@@ -84,11 +84,21 @@ const update = async (columnId, updateData) => {
   } catch (error) { throw new Error(error) }
 }
 
+const deleteOneByColumnId = async (columnId) => {
+  try {
+    const result = await GET_DB().collection(COLUMN_COLLECTION_NAME).deleteOne({
+      _id: new ObjectId(columnId)
+    })
+    return result
+  } catch (error) { throw new Error(error) }
+}
+
 export const columnModel = {
   COLUMN_COLLECTION_NAME,
   COLUMN_COLLECTION_SCHEMA,
   createNew,
   findOneById,
   pushCardOrderIds,
-  update
+  update,
+  deleteOneByColumnId
 }
